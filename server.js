e500850cb7eebadf62786474dcaec31a171d7861
paragasu <jeffry.lunggot@trimajasa.com>
2016-09-29 16:59:33 +0800

    use express middleware for form validation

--- server.js
+++ server.js
@@ -2,31 +2,30 @@
 
 const config = require('./config')
 const form = require('./lib/validate');
-const payment = require('./lib/payment');
+const pay = require('./lib/payment');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:true}))
+app.use('/checkout', form.checkValid);
 
 app.post('/checkout', (req, res) => {
-  if(form.isValid(req.body)){
-    //record transaction
-    let payment = payment({
-        currency: req.body.cc_currency,
-        name: req.body.cc_name,
-        number: req.body.cc_number,
-        expirationMonth: req.body.cc_expiry_month,
-        expirationYear: req.body.cc_expiry_year,
-        cvv: req.body.cc_cvv   
-    })
+  //record transaction
+  let payment = pay({
+      currency: req.body.cc_currency,
+      name: req.body.cc_name,
+      number: req.body.cc_number,
+      expirationMonth: req.body.cc_expiry_month,
+      expirationYear: req.body.cc_expiry_year,
+      cvv: req.body.cc_cvv   
+  })
 
-    payment.send(req.body.cc_price, (err, res) => {
-      if(err) throw err;
-      console.log(res);
-    })    
-  }
+  payment.send(req.body.cc_price, (err, res) => {
+    if(err) throw err;
+    console.log(res);
+  })    
 
   res.send('hello')
 })
